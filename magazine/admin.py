528e8befc1d77a5ce0c638ee9612fa66275b309a
Pavlin Gergov <gergov.pavlin@gmail.com>
2017-05-06 18:24:28 +0300

    Add magazine & articles

--- magazine/admin.py
+++ magazine/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from magazine.models import Magazine, Article
+
+
+@admin.register(Magazine)
+class MagazineAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'magazine', 'author')
--- magazine/models.py
+++ magazine/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Author
+
+
+class Magazine(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    magazine = models.ForeignKey(Magazine)
+    author = models.ForeignKey(Author)
+
+    def __str__(self):
+        return "Article {} written by {}".format(self.title, self.author)
