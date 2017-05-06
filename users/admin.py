659b3f4fe19d6cf4176fb0c754ccaf68e3b38a0d
Pavlin Gergov <gergov.pavlin@gmail.com>
2017-05-06 18:23:57 +0300

    Add base user, author & buyer

---
+++ users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from users.models import Author, Buyer
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'age', 'interests')
+    fields = ('email', 'age', 'interests')
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'came_from', )
+    fields = ('email', 'came_from')
---
+++ users/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from .managers import BaseUserManager
+
+
+class BaseUser(PermissionsMixin, AbstractBaseUser):
+    email = models.EmailField(max_length=255,
+                              unique=True,
+                              verbose_name='email address')
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = BaseUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+
+class Author(BaseUser):
+    age = models.IntegerField()
+    interests = models.TextField(blank=True, null=True)
+
+
+class Buyer(BaseUser):
+    came_from = models.CharField(max_length=255, blank=True)
