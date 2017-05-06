afe8a35d0651721a8311974ae3f17fa6220bb063
Pavlin Gergov <gergov.pavlin@gmail.com>
2017-05-07 01:37:51 +0300

    Add buy article view

--- magazine/urls.py
+++ magazine/urls.py
@@ -1,6 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
-from .views import ArticleList, MagazineList
+from .views import ArticleList, MagazineList, BuyArticleView
+
+
+article_patterns = [
+    url(
+        regex='^$',
+        view=ArticleList.as_view(),
+        name='list'
+    ),
+    url(
+        regex='^buy$',
+        view=BuyArticleView.as_view(),
+        name='buy'
+    )
+]
 
 
 urlpatterns = [
@@ -10,8 +24,10 @@ urlpatterns = [
         name='magazine-list'
     ),
     url(
-        regex='^(?P<magazine_id>[0-9]+)/article$',
-        view=ArticleList.as_view(),
-        name='article-list'
+        regex='^(?P<magazine_id>[0-9]+)/article/',
+        view=include(
+            arg=article_patterns,
+            namespace='article'
+        )
     )
 ]
--- magazine/views.py
+++ magazine/views.py
@@ -1,17 +1,30 @@
+from django.views import View
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
 
+from .tasks import charge
 from .models import Article, Magazine
 
 
 class MagazineList(LoginRequiredMixin, ListView):
     model = Magazine
-    fields = ('name', )
 
 
 class ArticleList(LoginRequiredMixin, ListView):
     model = Article
-    fields = ('title', 'author')
 
     def get_queryset(self):
-        return self.model.objects.filter(magazine_id=self.kwargs['magazine_id'])
+        return self.model.objects\
+                         .filter(magazine_id=self.kwargs['magazine_id'])
+
+
+class BuyArticleView(LoginRequiredMixin, View):
+    def get_success_url(self):
+        return reverse('magazine:article:list', kwargs=self.kwargs)
+
+    def post(self, request, *args, **kwargs):
+        charge.delay()
+
+        return redirect(self.get_success_url())
